b97fc2c78fd28ac3033e39d505c54de7236e0e71
Danilovisk <danilo05.mesquita@gmail.com>
2022-05-19 10:00:08 -0300

    Validação de user e ProfileController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1660780800&v=beta&t=NV5YvAiR1spFGWoXyHLJdEPFO05b-bUVSYueiq3dvzs" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>LaravelGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>123</strong> posts</div>
+                <div class="pr-5"><strong>456</strong> followers</div>
+                <div class="pr-5"><strong>789</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">LaravelGram</div>
+            <div>We're a global community of millions of people learning to code together.</div>
+            <div><a href="#">LaravelGram</a></div>    
+        </div> 
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1660780800&v=beta&t=NV5YvAiR1spFGWoXyHLJdEPFO05b-bUVSYueiq3dvzs" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1660780800&v=beta&t=NV5YvAiR1spFGWoXyHLJdEPFO05b-bUVSYueiq3dvzs" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1660780800&v=beta&t=NV5YvAiR1spFGWoXyHLJdEPFO05b-bUVSYueiq3dvzs" class="w-100">
+        </div>
+    </div>   
+</div>
+@endsection
