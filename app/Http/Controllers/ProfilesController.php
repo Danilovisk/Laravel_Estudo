43fad00d61b379ee69debf87f1b835e00d4c535c
Danilovisk <danilo05.mesquita@gmail.com>
2022-05-22 15:06:49 -0300

    Editar perfil

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,11 +6,27 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index($user)
+    public function index(\App\User $user)
     {
-       $user = \App\User::findOrFail($user);
-        return view('profiles.index', [
-            'user' => $user,
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(\App\User $user)
+    {
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(\App\User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
         ]);
+
+        auth()->user()->profile()->update($data);
+
+        return redirect("/profile/{$user->id}");
     }
 }
