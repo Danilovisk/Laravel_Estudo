74bdc42edb445eb4102f90e431fa1047b9c56115
Danilovisk <danilo05.mesquita@gmail.com>
2022-05-22 16:10:31 -0300

    Finalizar Edição de Perfil

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,13 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
-        $user->profile()->create();
-        return $user;
+
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,23 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1660780800&v=beta&t=NV5YvAiR1spFGWoXyHLJdEPFO05b-bUVSYueiq3dvzs" class="rounded-circle">
+            <img src="/storage/{{$user->profile->image}}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add New Post</a>
+
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
+
+                @endcan
+
             </div>
-            <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
+
             <div class="d-flex">
                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>456</strong> followers</div>
